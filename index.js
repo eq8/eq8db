89c741e23155431dd23c79bb98b15d0b5c2338ec
bbartolome <bbartolome@users.noreply.github.com>
2017-05-15 15:54:02 -0400

    refactor: create a `getLogger` function

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
 'use strict';
 
-const winston = require('winston');
-const Logger = winston.Logger;
+const _ = require('lodash');
+const lib = require('./lib/index.js');
 
 module.exports = function eq8store(options) {
-	const logger = new Logger({
-		levels: {
-			trace: 5,
-			debug: 4,
-			info: 3,
-			warn: 2,
-			error: 1,
-			fatal: 0
-		},
-		colors: {
-			trace: 'grey',
-			debug: 'blue',
-			info: 'green',
-			warn: 'yellow',
-			error: 'red',
-			fatal: 'magenta'
-		},
-		transports: [
-			new (winston.transports.Console)({
-				level: options.logLevel || 'info',
-				colorize: true
-			})
-		]
-	});
+	const logger = lib.getLogger(_.get(options, 'args.logLevel'));
 
-	logger.info('options:', options);
+	logger.debug('options:', options);
 };
