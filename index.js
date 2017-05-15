af33e6a53650f3618699ab87eca32e45a7d8dcd9
bbartolome <bbartolome@users.noreply.github.com>
2017-05-15 06:38:34 -0400

    feat: add winston logger

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 'use strict';
 
+const winston = require('winston');
+const Logger = winston.Logger;
+
 module.exports = function eq8store(options) {
-	console.log('options:', options);
+	const logger = new Logger({
+		levels: {
+			trace: 5,
+			debug: 4,
+			info: 3,
+			warn: 2,
+			error: 1,
+			fatal: 0
+		},
+		colors: {
+			trace: 'grey',
+			debug: 'blue',
+			info: 'green',
+			warn: 'yellow',
+			error: 'red',
+			fatal: 'magenta'
+		},
+		transports: [
+			new (winston.transports.Console)({
+				level: options.logLevel || 'info',
+				colorize: true
+			})
+		]
+	});
+
+	logger.info('options:', options);
 };
