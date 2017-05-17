4be8103844885810825c0a1a4ccf6b5458b6950f
bbartolome <bbartolome@users.noreply.github.com>
2017-05-16 22:03:32 -0400

    feat: add package info on run

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const _ = require('lodash');
+
+const pkg = require('./package.json');
+const NAME = _.get(pkg, 'name');
+const VERSION = _.get(pkg, 'version');
+
 const lib = require('./lib/index.js');
 
 module.exports = function eq8store(options) {
 	const logger = lib.getLogger(_.get(options, 'args.logLevel'));
 
-	logger.debug('options:', options);
+	logger.info(`${NAME}#v${VERSION}:`, options);
 };
