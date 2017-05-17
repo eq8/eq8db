2f65aeae2aca2221d5a167e99522d4a9bab53d49
bbartolome <bbartolome@users.noreply.github.com>
2017-05-16 22:57:01 -0400

    fix: add stub for run process

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ const VERSION = _.get(pkg, 'version');
 
 const lib = require('./lib/index.js');
 
-module.exports = function eq8store(options) {
+module.exports = function bootstrap(options) {
 	const logger = lib.getLogger(_.get(options, 'args.logLevel'));
 
 	logger.info(`${NAME}#v${VERSION}:`, options);
+
+	const kernel = require('./lib/kernel.js')(logger);
+
+	const proc = _.get(kernel, options.cmd) || (() => {
+		logger.error(`Command ${options.cmd} not found!`);
+	});
+
+	proc(options);
 };
