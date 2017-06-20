c1c6b19606b3fb1f0d4087beb54e7457f0882a2d
bbartolome <bbartolome@users.noreply.github.com>
2017-06-19 23:26:05 -0400

    fix: redo binaries

--- index.js
+++ index.js
@@ -2,5 +2,22 @@
 
 const pkg = require('./package.json');
 const createInstance = require('./lib');
+const instance = createInstance(pkg);
 
-module.exports = createInstance(pkg);
+module.exports = {
+	listen: options => {
+		instance('listen', options);
+	},
+	start: options => {
+		instance('start', options);
+	},
+	deploy: options => {
+		instance('deploy', options);
+	},
+	teardown: options => {
+		instance('teardown', options);
+	},
+	status: options => {
+		instance('status', options);
+	}
+};
