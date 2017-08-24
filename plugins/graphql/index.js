2e6901f14699055af8a9fcb15528b3d7b9eef95f
bbartolome <bbartolome@users.noreply.github.com>
2017-08-24 19:51:29 -0400

    fix: move plugins/graphql/db up one directory

---
+++ plugins/graphql/index.js
@@ -0,0 +1,51 @@
+/* globals Promise */
+
+'use strict';
+
+const plugin = 'graphql';
+
+const _ = require('lodash');
+const { makeExecutableSchema } = require('graphql-tools');
+const graphqlHTTP = require('express-graphql');
+
+// TODO: replace me with actual dynamic type definitions and resolvers
+const tmpTypeDefs = `
+type Query {
+	up: Boolean
+}
+`;
+const tmpResolvers = {
+	Query: {
+		up: () => Promise.resolve(true)
+	}
+};
+
+module.exports = function createGraphQLPlugin({ logger }) {
+	return function graphqlPlugin() {
+		const services = this;
+
+		logger.debug('graphqlPlugin', __filename);
+
+		/**
+		 * returns the middleware other than the admin API
+		 */
+		services.add({ plugin, q: 'middleware' }, (args, done) => {
+
+			/*
+			 * TODO
+			 * - construct schema based from database
+			 * - construct rootValue from database to queue messages for the worker process
+			 * - create a middleware and store in an LRU cache
+			 *   - use the domain and schema version as the key
+			 */
+
+			const schema = makeExecutableSchema({ typeDefs: tmpTypeDefs, resolvers: tmpResolvers, logger });
+
+			const middleware = graphqlHTTP({ schema, rootValue: {}, graphiql: true });
+
+			done(null, {
+				middleware
+			});
+		});
+	};
+};
--- plugins/index.js
+++ plugins/index.js
@@ -5,7 +5,9 @@ module.exports = function pluginsLoader(commons) {
 
 	logger.debug('pluginsLoader', __filename);
 
-	return function loadPlugins({ action, docker, apiPath, port, store, dev }, done) {
+	return function loadPlugins({
+		action, docker, domain, apiPath, port, store, dev
+	}, done) {
 		switch (action) {
 		case 'deploy':
 		case 'teardown':
@@ -17,8 +19,7 @@ module.exports = function pluginsLoader(commons) {
 		case 'serve':
 		default:
 			framework.use(require('./store')(commons), { store });
-			framework.use(require('./graphql/admin')(commons));
-			framework.use(require('./graphql/db')(commons));
+			framework.use(require('./graphql')(commons), { domain });
 			framework.use(require('./server')(commons), { apiPath, port });
 			break;
 		}
