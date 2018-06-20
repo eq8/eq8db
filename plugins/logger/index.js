b5255172bc909746185edf99bfee64c98df66b36
bbartolome <bbartolome@users.noreply.github.com>
2018-06-19 20:51:39 -0400

    fix: update to winston breaking change

--- plugins/logger/index.js
+++ plugins/logger/index.js
@@ -2,19 +2,9 @@
 'use strict';
 
 define(['winston'], winston => {
-	const { Logger } = winston;
-
-	const plugin = new Logger({
+	const plugin = winston.createLogger({
 		levels: {
 			trace: 5, debug: 4, info: 3, warn: 2, error: 1, fatal: 0
-		},
-		colors: {
-			trace: 'grey',
-			debug: 'blue',
-			info: 'green',
-			warn: 'yellow',
-			error: 'red',
-			fatal: 'magenta'
 		},
 		transports: [
 			new (winston.transports.Console)({
