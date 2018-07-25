ec811a553a2175c11359fc778713d15e950c0796
bbartolome <bbartolome@users.noreply.github.com>
2018-07-25 19:33:40 -0400

    refactor: subscribe to store changes

--- plugins/store/index.js
+++ plugins/store/index.js
@@ -1,12 +1,31 @@
-/* global define */
+/* global define, Promise */
 'use strict';
 
 define([
 	'lodash',
+	'lru-cache',
+	'immutable',
 	'url-parse',
 	'rethinkdb',
 	'-/logger/index.js'
-], (_, parse, r, logger) => {
+], (_, lru, { Map }, parse, r, logger) => {
+	let cursors = Map({});
+
+	const max = !_.isNaN(parseInt(process.env.MVP_STORE_LRU_MAXSIZE, 10))
+		? parseInt(process.env.MVP_STORE_LRU_MAXSIZE, 10)
+		: 500;
+	const maxAge = !_.isNaN(parseInt(process.env.MVP_STORE_LRU_MAXAGE, 10))
+		? parseInt(process.env.MVP_STORE_LRU_MAXAGE, 10)
+		: 1000 * 60 * 60;
+	const cache = lru({
+		max,
+		maxAge,
+		dispose(key) {
+			(cursors.get(key) || { close: _.noop }).close();
+		},
+		noDisposeOnSet: true
+	});
+
 	let conn;
 
 	const plugin = {
@@ -60,9 +79,35 @@ define([
 			return q.run(conn);
 		},
 		read({ type, id }) {
-			return r.table(type)
-				.get(id)
-				.run(conn);
+			const key = `${type}/${id}`;
+			const value = cache.get(key);
+
+			return value
+				? Promise.resolve(value)
+				: new Promise((resolve, reject) => {
+					r.table(type)
+						.get(id)
+						.changes({ includeInitial: true, squash: true })
+						.run(conn, (err, cursor) => {
+							if (err) {
+								return reject(err);
+							}
+
+							cursors = cursors.set(key, cursor);
+
+							let unresolved = true;
+
+							return cursor.each((eachErr, row) => {
+								const { new_val: v } = row || {}; // eslint-disable-line camelcase
+
+								cache.set(key, v);
+								if (unresolved) {
+									unresolved = false;
+									resolve(v);
+								}
+							});
+						});
+				});
 		},
 		edit({ type, object }) {
 			return r.table(type)
