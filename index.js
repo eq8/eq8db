2ee287a010fc540964da8d79ffb33b4259b1fa02
bbartolome <bbartolome@users.noreply.github.com>
2018-07-29 11:08:18 -0400

    fix: enable exporting the library

--- index.js
+++ index.js
@@ -1,46 +1,17 @@
-/* global Promise */
 'use strict';
 
 const _ = require('lodash');
 const path = require('path');
-const rjs = require('requirejs');
+const boot = require('@eq8/mvp-boot');
 
-const { version } = require('./package.json');
-
-module.exports = function mvp(argv) {
-
-	const settings = _.defaultsDeep(argv, {});
-
-	const { overrides } = settings;
-
-	const mapOverrides = overrides
-		? overrides
-		: {};
-
-	const map = _.defaultsDeep({}, mapOverrides, {
-		'*': {
-			'-': path.join(__dirname, './plugins')
+module.exports = function mvp(args) {
+	const config = _.defaultsDeep({}, args, {
+		overrides: {
+			'*': {
+				'-/ext/controller': path.join(__dirname, './lib/ext/controller')
+			}
 		}
 	});
 
-	// Leverage AMD for the plugin architecture
-	// We'll be using the `overrides` to re-map the implementation of internal dependencies
-	rjs.config({ map });
-
-	return new Promise(resolve => {
-		rjs([
-			'-/options/index.js',
-			'-/logger/index.js',
-			'-/store/index.js',
-			'-/server/index.js'
-		], (options, logger, store, server) => {
-			resolve({
-				version,
-				options,
-				logger,
-				store,
-				server
-			});
-		});
-	});
+	return boot(config);
 };
